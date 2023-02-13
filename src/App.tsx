d2a4aed5dde1a6da1aa89e600d1c349a7aebd6a2
CrAvila <00032420@uca.edu.sv>
2023-02-13 00:41:39 -0600

    :construction: fix: canvas positioning

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import { Suspense } from "react";
 import { Earth } from 'components/earth/earth';
 
 const CanvasContainer = styled.div`
-  width: 100vh;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 `;
 
 
--- src/components/earth/earth.tsx
+++ src/components/earth/earth.tsx
@@ -6,6 +6,8 @@ import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg"
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg"
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
 
 
 
@@ -16,11 +18,27 @@ export function Earth(props: any){
 
     return( 
     <>
-    <ambientLight intensity={0.5}/>
+    <ambientLight intensity={1}/>
+    <mesh>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial 
+        map={cloudsMap} 
+        opacity={0.4} 
+        depthWrite={true} 
+        transparent={true} 
+        side={THREE.DoubleSide}/>
+    </mesh>
         <mesh>
             <sphereGeometry args={[1, 32, 32]}/>
             <meshPhongMaterial specularMap={specularMap}/>
             <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+            <OrbitControls 
+            enableZoom={true} 
+            enablePan={true}
+            zoomSpeed={0.6}
+            panSpeed={0.5}
+            rotateSpeed={0.4}
+            />
         </mesh>
     </>
     )
