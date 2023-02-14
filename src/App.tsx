4c1ccd81c018c5d1cca4abcb65b2382db7b21d77
Yury Rivera <yrivera@applaudostudios.com>
2023-02-14 10:29:37 -0600

    fixing PR comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import { Outlet } from 'react-router-dom';
 import { ErrorBoundary } from 'components';
 import 'antd/dist/reset.css';
 import './App.scss';
-import styled from "styled-components"
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from "react";
-import { Earth } from 'components/earth/earth';
-import React from 'react';
-import { World } from 'components/Globe/globe';
-
-const CanvasContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
 
 export function App(): JSX.Element {
   return (
-          <World />
+    <Layout className="layout">
+      <Layout.Header className="layout-header">
+        <img src={logo} alt="GAF energy logo" className="logo" width={40} />
+      </Layout.Header>
+      <Layout.Content>
+        <ErrorBoundary>
+          <Outlet />
+        </ErrorBoundary>
+      </Layout.Content>
+    </Layout>
   );
 }
