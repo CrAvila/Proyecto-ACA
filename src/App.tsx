96b25d5f7feaa7677d3132c6cf3b974f57b83938
CrAvila <00032420@uca.edu.sv>
2023-02-14 21:57:40 -0600

    :tada: add: routes to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Layout } from 'antd';
 import logo from 'assets/react.svg';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { ErrorBoundary } from 'components';
 import 'antd/dist/reset.css';
 import './App.scss';
@@ -9,7 +9,15 @@ export function App(): JSX.Element {
   return (
     <Layout className="layout">
       <Layout.Header className="layout-header">
-        <img src={logo} alt="GAF energy logo" className="logo" width={40} />
+        <div className="navbar">
+        <img src={logo} alt="CAP logo" className="logo" width={40} />
+          <div className='logo-title'>CAP</div>
+           <ul className="nav-links">
+              <Link to="/">Home</Link>
+              <Link to="/charts">Charts</Link>
+              <Link to="/about">About</Link>
+           </ul>
+        </div>
       </Layout.Header>
       <Layout.Content>
         <ErrorBoundary>
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,6 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 import { App } from 'App';
-import { Home, Login, NotFound } from 'pages';
+import { Home, Login, NotFound, About, Charts } from 'pages';
 
 export const routes: RouteObject = {
   element: <App />,
@@ -9,6 +9,8 @@ export const routes: RouteObject = {
     { path: '/', element: <Home /> },
     { path: '/home', element: <Home /> },
     { path: '/login', element: <Login /> },
+    { path: '/about', element: <About />},
+    { path: '/charts', element: <Charts />},
     { path: '*', element: <NotFound /> }
   ]
 };
