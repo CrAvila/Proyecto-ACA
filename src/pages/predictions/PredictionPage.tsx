f9977899de2bce996e1c93e6a7bc30d33894d804
CrAvila <00032420@uca.edu.sv>
2024-07-07 17:02:50 -0600

    feat: map handler event :tada:

--- src/pages/predictions/PredictionPage.tsx
+++ src/pages/predictions/PredictionPage.tsx
@@ -1,8 +1,7 @@
-// src/pages/predictions/PredictionPage.tsx
 import React, { useState } from 'react';
 import { Layout, Menu, Form, Input, Button } from 'antd';
 import { Link, useLocation } from 'react-router-dom';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { ErrorBoundary } from 'components';
 import { ToastContainer } from 'react-toastify';
@@ -13,7 +12,7 @@ import './PredictionPage.scss';
 
 const PredictionPage = () => {
   const route = useLocation();
-  const [filterOptions, setFilterOptions] = useState({});
+  const [filterOptions, setFilterOptions] = useState({ latitude: 0, longitude: 0, depth: 0 });
   const [predictionData, setPredictionData] = useState([]);
 
   const center = [13.794185, -88.89653];
@@ -31,20 +30,37 @@ const PredictionPage = () => {
     setPredictionData(predictions.arraySync());
   };
 
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        setFilterOptions(prev => ({ ...prev, latitude: lat, longitude: lng }));
+      },
+    });
+
+    return filterOptions.latitude !== 0 && filterOptions.longitude !== 0 ? (
+      <Marker position={[filterOptions.latitude, filterOptions.longitude]}>
+        <Popup>
+          Latitude: {filterOptions.latitude}, Longitude: {filterOptions.longitude}
+        </Popup>
+      </Marker>
+    ) : null;
+  };
+
   return (
     <Layout className="layout">
       <Layout.Content>
         <ErrorBoundary>
           <div className="prediction-container">
-            <Form name="filter-form" layout="inline" onFinish={onFinish}>
+            <Form name="filter-form" layout="inline" onFinish={onFinish} initialValues={filterOptions}>
               <Form.Item className="form-item" name="latitude" label="Latitude">
-                <Input type="number" step="0.01" />
+                <Input value={filterOptions.latitude} type="number" step="0.01" />
               </Form.Item>
               <Form.Item className="form-item" name="longitude" label="Longitude">
-                <Input type="number" step="0.01" />
+                <Input value={filterOptions.longitude} type="number" step="0.01" />
               </Form.Item>
               <Form.Item className="form-item" name="depth" label="Depth">
-                <Input type="number" step="0.1" />
+                <Input value={filterOptions.depth} type="number" step="0.1" />
               </Form.Item>
               <Form.Item>
                 <Button className="predict-btn" type="primary" htmlType="submit">
@@ -57,6 +73,7 @@ const PredictionPage = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               />
+              <LocationMarker />
               {predictionData.map((prediction, index) => (
                 <Marker key={index} position={[filterOptions.latitude, filterOptions.longitude]}>
                   <Popup>Predicted Magnitude: {prediction}</Popup>
