c6f9eba4e736d5495b02bc1e1cded097625d4ff3
CrAvila <00032420@uca.edu.sv>
2024-07-07 01:38:51 -0600

    fix: format or links :wrench:

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
 import { Layout, Menu } from 'antd';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import { ErrorBoundary } from 'components';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'antd/dist/reset.css';
 import './App.scss';
 
 export function App(): JSX.Element {
   const route = useLocation();
+
   return (
     <Layout className="layout">
       <Layout.Header className="layout-header">
         <div className="navbar">
-          <Link to="/" className='title'>QuakeSphere</Link>
-          <div className='right'>
+          <Link to="/" className="title" aria-label="QuakeSphere Home">
+            QuakeSphere
+          </Link>
+          <div className="right">
             <Menu theme="dark" mode="horizontal" selectedKeys={[route.pathname]}>
               <Menu.Item key="/">
-                <Link to="/">Main</Link>
+                <Link to="/" aria-label="Main Page">
+                  Main
+                </Link>
               </Menu.Item>
               <Menu.Item key="/home">
-                <Link to="/home">Home</Link>
+                <Link to="/home" aria-label="Home Page">
+                  Home
+                </Link>
               </Menu.Item>
               <Menu.Item key="/charts">
-                <Link to="/charts">Charts</Link>
+                <Link to="/charts" aria-label="Charts Page">
+                  Charts
+                </Link>
               </Menu.Item>
               <Menu.Item key="/about">
-                <Link to="/about">About</Link>
+                <Link to="/about" aria-label="About Page">
+                  About
+                </Link>
               </Menu.Item>
             </Menu>
           </div>
